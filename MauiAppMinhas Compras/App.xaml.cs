c4b02ce8523ef3ba0d46001fc78836423c4bf41a
rache <rache@DEEP_BLUE>
2025-08-23 19:02:10 -0300

    mudanças no front end

--- MauiAppMinhas Compras/App.xaml.cs
+++ MauiAppMinhas Compras/App.xaml.cs
@@ -1,7 +1,33 @@
-﻿namespace MauiAppMinhas_Compras
+﻿using MauiAppMinhas_Compras.Helpers;
+
+namespace MauiAppMinhas_Compras
 {
     public partial class App : Application
     {
+
+        static SQLiteDatabaseHelper _db;
+
+        public static SQLiteDatabaseHelper Db
+        {
+
+            get
+            {
+                if (_db == null)
+                {
+                    string path = Path.Combine(
+                        Environment.GetFolderPath(
+                            Environment.SpecialFolder.LocalApplicationData),
+                        "banco_sqlite_compras.db3");
+
+                    _db = new SQLiteDatabaseHelper(path);
+
+                }
+
+                return _db;
+            }
+
+        }
+
         public App()
         {
             InitializeComponent();
