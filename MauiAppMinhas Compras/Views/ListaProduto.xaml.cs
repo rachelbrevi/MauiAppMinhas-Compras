77a2060650bc8d952098a524247be5583bd9eb00
rache <rache@DEEP_BLUE>
2025-08-30 21:32:32 -0300

    lista

--- MauiAppMinhas Compras/Views/ListaProduto.xaml.cs
+++ MauiAppMinhas Compras/Views/ListaProduto.xaml.cs
@@ -1,11 +1,25 @@
+using MauiAppMinhas_Compras.Models;
+using System.Collections.ObjectModel;
+
 namespace MauiAppMinhas_Compras.Views;
 
 public partial class ListaProduto : ContentPage
 {
+
+	ObservableCollection<Produto> lista = new ObservableCollection<Produto>();
 	public ListaProduto()
 	{
 		InitializeComponent();
+
+		lst_produtos.ItemsSource = lista;
+
 	}
+
+    protected async override void OnAppearing()
+    {
+       List<Produto> tmp = await App.Db.GetAll();
+		tmp.ForEach ( i  => lista.Add(i));
+    }
 
     private void ToolbarItem_Clicked(object sender, EventArgs e)
     {
@@ -18,6 +32,32 @@ public partial class ListaProduto : ContentPage
 		{
 			DisplayAlert("Ops", ex.Message, "OK");
 		}
+
+    }
+
+    private async void txt_search_TextChanged(object sender, TextChangedEventArgs e)
+    {
+		string q = e.NewTextValue;
+
+		lista.Clear();
+
+        List<Produto> tmp = await App.Db.Search(q);
+        tmp.ForEach(i => lista.Add(i));
+
+
+    }
+
+    private void ToolbarItem_Clicked_1(object sender, EventArgs e)
+    {
+		double soma = lista.Sum(i  => i.Total);
+
+		string msg = $"O total é {soma:C}";
+
+		DisplayAlert("Total dos produtos", msg, "OK");
+    }
+
+    private void MenuItem_Clicked(object sender, EventArgs e)
+    {
 
     }
 }
